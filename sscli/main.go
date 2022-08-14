6ed20775d47e05fea5b577ff4d50b4a31ba279b0
Cyrus Kia <hckia1@gmail.com>
2022-08-14 14:12:46 -0400

    created starting point for cobra-cli. Added Data directory to hold swatches, products, and a gitignore within to not store image, excel, or csv files

--- sscli/main.go
+++ sscli/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/hckia/shopify-seller-central-product-converter/sscli"
+import (
+	"github.com/hckia/shopify-seller-central-product-converter/sscli/cmd/sscli"
+)
 
 func main() {
 	sscli.Execute()
